2599c213c638a1c2171390f435e147d7bc0b3d5f
juliu-cesar <juliucesarcardoso@gmail.com>
2023-11-29 10:29:50 -0300

    ci: test report path folder | add another tests to sum.go

--- SonarQube/SonarCloud/sum_test.go
+++ SonarQube/SonarCloud/sum_test.go
@@ -8,3 +8,27 @@ func TestSum(t *testing.T){
 		t.Error("Result must be 5")
 	}
 }
+func TestSub(t *testing.T){
+	result := sub(5,3)
+	if result != 2{
+		t.Error("Result must be 2")
+	}
+}
+func TestTimes(t *testing.T){
+	result := times(4,3)
+	if result != 12{
+		t.Error("Result must be 12")
+	}
+}
+func TestDivide(t *testing.T){
+	result := divide(9,3)
+	if result != 3{
+		t.Error("Result must be 3")
+	}
+}
+func TestSumX(t *testing.T){
+	result := sumX(1,3,5)
+	if result != 9{
+		t.Error("Result must be 9")
+	}
+}
